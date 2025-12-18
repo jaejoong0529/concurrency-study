626163da086933ce57d07da118d40425d7af82e1
kimjaejoong <wcn07003@naver.com>
2025-12-18 22:40:53 +0900

    first commit

---
+++ src/test/java/com/concurrencystudy/ConcurrencyStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.concurrencystudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrencyStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
